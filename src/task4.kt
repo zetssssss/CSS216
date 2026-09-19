dd2a541a85b0bbe7a014c4bc0a58f71f2d4aacd2
zetssssss <240103020@sdu.edu.kz>
2026-09-19 17:59:06 +0500

    HW1: Task4

--- src/task4.kt
+++ src/task4.kt
@@ -1,6 +1,6 @@
 fun main() {
-    val numberOfAdults = 20
-    val numberOfKids = 30
-    val total = numberOfAdults + numberOfKids
-    println("The total party size is: $total")
+    val baseSalary = 5000
+    val bonusAmount = 1000
+    val totalSalary = baseSalary + bonusAmount
+    println("Congratulations for your bonus! You will receive a total of $totalSalary (additional bonus).")
 }
