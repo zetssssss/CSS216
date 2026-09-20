19cde636e3c19f0b0d4af0858b4f09b0f84eff9a
zetssssss <240103020@sdu.edu.kz>
2026-09-20 15:01:27 +0500

    HW2: task2

---
+++ src/HW2.task2.kt
@@ -0,0 +1,21 @@
+fun main() {
+    val child = 5
+    val adult = 28
+    val senior = 87
+
+    val isMonday = true
+
+    println("The movie ticket price for a person aged $child is \$${ticketPrice(child, isMonday)}.")
+    println("The movie ticket price for a person aged $adult is \$${ticketPrice(adult, isMonday)}.")
+    println("The movie ticket price for a person aged $senior is \$${ticketPrice(senior, isMonday)}.")
+}
+
+fun ticketPrice(age: Int, isMonday: Boolean): Int {
+    return when {
+        age < 0 || age > 100 -> -1
+        age <= 12 -> 15
+        age <= 60 && isMonday -> 25
+        age <= 60 -> 30
+        else -> 20
+    }
+}
